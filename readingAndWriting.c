bd57496349e0d52153efd49c309a41082e1d7386
tylerwarwick <98790720+tylerwarwick@users.noreply.github.com>
2022-11-11 17:34:51 -0500

    Encryption/Decryption done
    Can now open and close files to both read and write to. Encryption works but decryption won't run because CRP files won't open on mac

--- readingAndWriting.c
+++ readingAndWriting.c
@@ -8,24 +8,34 @@ FILE* openInputFile(char* n);
 //Opens file for us to write to
 FILE* openNewFile(char* n);
 
-//Encrypts file
-void encryptFile(FILE* read, FILE* write);
+int hexStringToInt(char c);
 
-//Decrypts File
-void decryptFile(FILE* k);
+//Converts hex to ASCII values
+int hexStringToDecimal(char c1, char c2);
 
-
+//Encrypts and decrypts files
+FILE* encryptFile(FILE* inputFile, FILE* outputFile);
+FILE* decryptFile(FILE* inputFile, FILE* outputFile);
 
 
 int main(void)
 {
-    char* sampleTextFile = ("text.txt");
-    char* sampleNewFile = ("posttext4.txt");
+    char* sampleTextFile = ("wordtest.txt");
+    char* sampleNewFile = ("postEncryption.crp");
+
+    char* encryptedFile = ("decryptionTest.crp");
+    char* decryptedFile = ("postDecryption.txt");
 
     FILE* inputFile = openInputFile(sampleTextFile);
     FILE* newFile = openNewFile(sampleNewFile);
 
-    encryptFile(inputFile, newFile);
+    FILE* inputFile2 = openInputFile(encryptedFile);
+    FILE* newFile2 = openInputFile(decryptedFile);
+
+    
+    FILE* x = encryptFile(inputFile, newFile);
+    //FILE* x = decryptFile();
+
 
     fclose(inputFile);
     fclose(newFile);
@@ -37,7 +47,7 @@ FILE* openNewFile(char* n)
     FILE * writingFile = fopen(n, "w");
     if (writingFile == NULL) 
     {
-        printf("New file couldn't be opened\n");
+        printf("Output file couldn't be opened\n");
     }
 
     return writingFile;
@@ -57,22 +67,117 @@ FILE* openInputFile(char* n)
     
 }   
 
-void encryptFile(FILE* read, FILE* write)
-{
-    char c;
-
-    while ((c = fgetc(read)) != EOF) 
+int hexStringToInt(char c)
     {
-        int outChar = c; /*= ((int)c - 16);
+    if (c >= 97) c = c - 32;
 
-        if (outChar < 32)
+    int first = c / 16 - 3;
+    int second = c % 16;
+    int result = first * 10 + second;
+    if (result > 9) result--;
+    return result;
+    
+    }
+
+int hexStringToDecimal(char c1, char c2)
+{
+    int newc1 = hexStringToInt(c1) * 16;
+    int newc2 = hexStringToInt(c2);
+    int result = newc1 + newc2;
+    return result;
+}
+
+
+FILE* encryptFile(FILE* inputFile, FILE* outputFile)
+{
+    char lineContents[121];
+    char* newLine= "\n";
+
+    //Get a line from the file until end of file
+    while (fgets(lineContents, 120, inputFile) != NULL)
+    {   
+        //Parse over every character of line 
+        for (int i = 0; lineContents[i] != '\0'; i++) 
         {
-            outChar = ((outChar - 32) + 144);
-        }
-        */
-        
-        fputc(outChar, write);
+            int outChar;
+            char c = lineContents[i];
+            char* tab = "TT";
 
+            //Special cases for tab and carriage return
+            if (c == 6 || c == 9 || c == 10 || c == 13)
+            {
+
+            //If input is tab then write TT
+                if (c == 9)
+                {
+                    fprintf(outputFile, "%s", tab);
+                }
+
+            /*
+            Ignore acknowledgement, line feed and carriage return
+            Since we are processing line by line we don't need to use these as indicators
+            We will just print a newline character after processing each line
+            */
+                else if (c == 6 || c == 10 || c == 13)
+                {
+                    continue;
+                }
+            }
+
+            //Otherwise apply encryption key
+           else
+            {
+                outChar = (c - 16);
+
+                if (outChar < 32)
+                {
+                    outChar = (outChar - 32) + 144;
+                }
+
+                fprintf(outputFile, "%X", outChar);
+            }
+        }
+
+        fprintf(outputFile, "%s", newLine);
     }
     
+    return outputFile;
+}
+
+FILE* decryptFile(FILE* inputFile, FILE* outputFile)
+{   
+    char* tab = "\t";
+    char lineContents[256];
+    char* newLine= "\n";
+
+    //Get a line from the file until end of file
+    while (fgets(lineContents, 120, inputFile) != NULL)
+    {   
+        //Parse over every character of line 
+        for (int i = 0; lineContents[i] != '\0'; i += 2) 
+        {
+            if (lineContents[i] == 'T' && lineContents[i + 1] == 'T') 
+            {
+                fprintf(outputFile, "%s", tab);
+            }
+
+            else
+            {
+                int outChar;
+                outChar = hexStringToDecimal(lineContents[i], lineContents[i + 1]);
+                outChar += 16;
+
+                if (outChar > 127)
+                {
+                    outChar = (outChar - 144) + 32;
+                }
+
+                fprintf(outputFile, "%c", outChar);
+            
+            }
+
+            fprintf(outputFile, "%s", newLine);
+        }
+    }   
+    return outputFile;
 }
